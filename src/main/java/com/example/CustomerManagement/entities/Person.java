bda5d3b3536f1b27f899018f4a65ee2922372a8d
Mariano Avancini <avancini16@gmail.com>
2024-12-09 15:37:10 -0300

    Create person entity

---
+++ src/main/java/com/example/CustomerManagement/entities/Person.java
@@ -0,0 +1,10 @@
+package com.example.CustomerManagement.entities;
+
+public class Person {
+    private Long id;
+    private String name;
+    private String lastname;
+    private int age;
+    private String email;
+    private int number;
+}
