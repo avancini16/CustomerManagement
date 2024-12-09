3e4d4c364d55fae377a478fae7081185abeaf9ed
Mariano Avancini <avancini16@gmail.com>
2024-12-09 18:00:16 -0300

    Add JPA annotations

--- src/main/java/com/example/CustomerManagement/entities/Person.java
+++ src/main/java/com/example/CustomerManagement/entities/Person.java
@@ -1,10 +1,41 @@
 package com.example.CustomerManagement.entities;
 
-public class Person {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "person")
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    @NotEmpty
     private String name;
+
+    @Column(nullable = false)
+    @NotEmpty
     private String lastname;
+
+    @Column(nullable = false)
+    @NotNull
     private int age;
+
+    @Column(nullable = false)
+    @NotEmpty
+    @Email
     private String email;
+
+    @Column(nullable = false)
+    @NotNull
     private int number;
 }
